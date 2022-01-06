45554e4d19d1479549d17aceffbae613776f6e43
Bujira <yurifcorrea@gmail.com>
2022-01-06 12:52:20 -0300

    commit 20: testing multer for uploads

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { getCategoryController } from "../modules/cars/useCases/getCategory";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +16,12 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return getCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.json({ message: "Success!" });
 });
 
 export { categoriesRoutes };
