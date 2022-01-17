47d9c7498798a87ac2eda7cd2d8025de571e6039
Bujira <yurifcorrea@gmail.com>
2022-01-17 16:31:48 -0300

    commit 39: users migration and entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,40 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  drivers_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
