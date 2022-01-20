e9259a3acff8488d4abe7e81bee45a60543863a8
Bujira <yurifcorrea@gmail.com>
2022-01-20 14:25:02 -0300

    commit 47: removing unecessary avatar files

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { User } from "../../entities/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { IUpdateUserAvatarDTO } from "../../typings/IUpdateUserAvatarDTO";
@@ -14,6 +15,10 @@ class UpdateUserAvatarUseCase {
   }
   async execute({ user_id, avatar_file }: IUpdateUserAvatarDTO): Promise<User> {
     const user = await this.usersRepository.getById({ id: user_id });
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
     user.avatar = avatar_file;
 
