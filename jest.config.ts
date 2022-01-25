32ef9944f9fd6e2a93773bdd6a371c1e184239b1
Bujira <yurifcorrea@gmail.com>
2022-01-25 12:58:12 -0300

    commit 49: correcting jest.config typo

--- jest.config.ts
+++ jest.config.ts
@@ -146,7 +146,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/*.spec.tsj"],
+  testMatch: ["**/*.spec.ts"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
