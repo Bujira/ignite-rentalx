246483b15829c31ca1cb497b6b758476fa5147e3
Bujira <yurifcorrea@gmail.com>
2022-01-25 13:16:37 -0300

    commit 50: unit test example

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create category", () => {
+  it("Should be able to create a new car category", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("Should not be able to create an existing car category", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
