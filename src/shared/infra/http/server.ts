c5ee60ac2ec545dfa61e9526f7046b88d428e824
Bujira <yurifcorrea@gmail.com>
2022-02-01 13:30:35 -0300

    commit 61: seeding an admin user into db

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,13 +4,14 @@ import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import { AppError } from "@shared/errors/AppError";
-
-import "../typeorm";
+import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
+
+createConnection();
 
 const app = express();
 
---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,21 @@
+import { hash } from "bcryptjs";
+import { v4 as uuid } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuid();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, drivers_license, "isAdmin", avatar, created_at)
+      values('${id}', 'admin', 'admin@rentalx.com', '${password}', '0123456789', true, '', 'now()')
+    `
+  );
+
+  await connection.close();
+}
+
+create().then(() => console.log("User admin created!"));
