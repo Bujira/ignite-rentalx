f863574c392a2c4022035127e3640846e00e325f
Bujira <yurifcorrea@gmail.com>
2022-02-14 13:18:51 -0300

    commit 79: test database config

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -3,7 +3,12 @@ import request from "supertest";
 import { app } from "@shared/infra/http/app";
 
 describe("Create Category Controller", () => {
-  it("test", async () => {
-    await request(app).get("cars/available").expect(200);
+  it("Should be able to create a new car category", async () => {
+    const response = await request(app).post("/categories").send({
+      name: "SuperTest - Category Name",
+      description: "SuperTest - Category Description",
+    });
+
+    expect(response.status).toBe(201);
   });
 });
