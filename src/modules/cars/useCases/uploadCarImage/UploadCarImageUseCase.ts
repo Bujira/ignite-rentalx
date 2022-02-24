52aaa6a46903d6fb5a3f00615e53eb04431ff06b
Bujira <yurifcorrea@gmail.com>
2022-02-24 16:59:54 -0300

    commit 88: upload car images documentation

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { CarImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarImagesRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
   image_names: string[];
@@ -10,10 +13,18 @@ interface IRequest {
 @injectable()
 class UploadCarImageUseCase {
   constructor(
+    @inject("CarsRepository")
+    private carsRepository: CarsRepository,
     @inject("CarImagesRepository")
     private carImagesRepository: CarImagesRepository
   ) { }
   async execute({ image_names, car_id }: IRequest): Promise<void> {
+    const car = await this.carsRepository.getById({ car_id })
+
+    if (!car) {
+      throw new AppError("Car does not exist!", 404);
+    }
+
     image_names.map(async (image) => {
       await this.carImagesRepository.create({
         image_name: image,
